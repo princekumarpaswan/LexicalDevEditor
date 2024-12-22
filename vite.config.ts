d1de8c338ef8c15e1acb32ee3339c0da61e6c4fe
Prince Kumar paswan <43513990+princekumarpaswan@users.noreply.github.com>
2024-12-22 23:28:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-
 export default defineConfig({
   plugins: [
     react(),
